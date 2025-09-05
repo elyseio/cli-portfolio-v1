1d421d6afec577086ece9c4aa6efae7e808ce553
elyseio <123453529+elyseio@users.noreply.github.com>
2025-09-05 13:41:01 +0800

    added commands and projects

---
+++ js/index.js
@@ -0,0 +1,47 @@
+const root = document.getElementById("root");
+
+const helpCommands = [
+    {
+        command: "about",
+        description: " - about me"
+    },
+    {
+        command: "projects",
+        description: " - my projects"
+    },
+    {
+        command: "clear",
+        description: " - clear terminal"
+    },
+    {
+        command: "email",
+        description: " - email me"
+    },
+    {
+        command: "github",
+        description: " - my github"
+    },
+    {
+        command: "banner",
+        description: " - display the banner"
+    },
+    {
+        command: "help",
+        description: " - list all commands"
+    },
+    {
+        command: "secret",
+        description: " - ?"
+    }
+];
+
+const projects = [
+    {
+        name: "gag_bot - Grow A Garden Roblox bot, GUI program built with python to automate purchasing gear or eggs",
+        link: "https://github.com/elyseio/gag_bot"
+    },
+    {
+        name: "CLI Portfolio - A terminal style portfolio buildt with vanilla HTML, CSS, and JavaScript.",
+        link: "https://elyseio.me"
+    }
+];
